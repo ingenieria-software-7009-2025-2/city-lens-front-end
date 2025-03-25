a6ede9a9fe760322ee53768149d8288b568f786b
Daniel Paredes <danielparedes@ciencias.unam.mx>
2025-03-24 23:08:02 -0600

    docs: implementaciones de Storybook

---
+++ src/components/ui/form/form.stories.tsx
@@ -0,0 +1,14 @@
+import {StoryFn, Meta} from '@storybook/react';
+import {Form} from './form';
+
+export default {
+    title: 'Componentes Básicos/Formulario',
+    component: Form,
+} as Meta<typeof Form>;
+
+const Template: StoryFn<typeof Form> = (args) => <Form {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+    children: 'Formulario Básico',
+};
